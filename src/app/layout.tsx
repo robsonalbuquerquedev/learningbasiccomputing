55a1261782cbcf10141ffb5c2838d62a4f87230c
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-09-28 01:33:40 -0300

    update: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+// Importando Header e Footer
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 export const metadata: Metadata = {
   title: "Learning Basic Computing",
-  description: "Um jeito divertido e simples de aprender informática! Descubra como os computadores funcionam e explore conteúdos criados especialmente para crianças.",
+  description:
+    "Um jeito divertido e simples de aprender informática! Descubra como os computadores funcionam e explore conteúdos criados especialmente para crianças.",
 };
 
 export default function RootLayout({
@@ -13,9 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
+      <body className="flex flex-col min-h-screen">
+        {/* Header fixo no topo */}
+        <Header />
+
+        {/* Conteúdo principal cresce para ocupar o espaço restante */}
+        <main className="flex-grow">{children}</main>
+
+        {/* Footer fixo no final */}
+        <Footer />
       </body>
     </html>
   );
