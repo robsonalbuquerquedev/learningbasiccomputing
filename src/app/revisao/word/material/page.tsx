a1bbff1aae1c23f7a0921373422b719c12b4747a
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-09-30 21:51:09 -0300

    update: updating revisao folder

---
+++ src/app/revisao/word/material/page.tsx
@@ -0,0 +1,9 @@
+export default function RevisaoPage() {
+    return (
+        <main className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-200 py-12 px-6">
+            <h1 className="text-4xl md:text-5xl font-bold text-center text-blue-900 mb-12">
+                Hello world!
+            </h1>
+        </main>
+    );
+}
